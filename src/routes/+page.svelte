0f6e1bff62b0bf32bde44427e0fb8e721a9a8bfc
0324bence <bence20050324@gmail.com>
2023-03-30 18:59:15 +0200

    Create basic layout and colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div id="main-container">
+    <div id="current-day-container">
+        <div class="header">a</div>
+        <div id="current-day">a</div>
+    </div>
+    <div id="month-container">a</div>
+    <div id="sidebar">a</div>
+</div>
+
+<style lang="scss">
+    @import "../assets/variables.scss";
+    #main-container {
+        background-color: $background-color;
+        color: $foreground-color;
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        align-items: stretch;
+    }
+    #current-day-container {
+        flex-grow: 1;
+        border-right: 1px solid $foreground-color;
+        display: flex;
+        flex-direction: column;
+
+        .header {
+            height: 40px;
+        }
+
+        #current-day {
+            flex-grow: 1;
+        }
+    }
+    #month-container {
+        flex-grow: 4;
+        border-right: 1px solid $foreground-color;
+    }
+    #sidebar {
+        width: 100px;
+    }
+
+    .header {
+        width: 100%;
+        border-bottom: 1px solid $foreground-color;
+    }
+</style>
